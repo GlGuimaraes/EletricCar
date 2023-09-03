c5250ecd72c98600f8c96836edfe54848def66dc
Guilherme <guimaraesss02@gmail.com>
2023-09-02 22:15:08 -0300

    Melhorando o código

--- app/src/main/java/com/example/eletriccar/CalcularAutonomia.kt
+++ app/src/main/java/com/example/eletriccar/CalcularAutonomia.kt
@@ -1,32 +1,41 @@
 package com.example.eletriccar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class CalcularAutonomia : AppCompatActivity() {
     lateinit var preco: EditText
     lateinit var kmPercorrido: EditText
-    lateinit var btnCalcular: Button
     lateinit var resultado: TextView
+    lateinit var btnCalcular: Button
+    lateinit var imageClose: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_calcular_autonomia)
+
         setupViews()
         setupListeners()
     }
+
     fun setupViews() {
         kmPercorrido = findViewById(R.id.edit_km_percorrido)
         preco = findViewById(R.id.edit_preco_Kwh)
         btnCalcular = findViewById(R.id.btn_calcular)
         resultado = findViewById(R.id.text_resultado)
+        imageClose = findViewById(R.id.imageClose)
     }
     fun setupListeners() {
         btnCalcular.setOnClickListener {
             calcular()
+        }
+        imageClose.setOnClickListener{
+            finish()
         }
     }
     fun calcular() {
